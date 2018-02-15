525483f232de3534dbf04b4dc738363fb8b4b1ba
Maxime Allanic <maxime.g.allanic@gmail.com>
2018-02-15 15:39:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,13 @@ var es        = require('event-stream'),
 
 function manifest(options) {
   options = options || {};
-  var contents = [];
-  contents.push('CACHE MANIFEST');
+  var contents = {
+      files: []
+  };
 
   var filename = options.filename || 'app.manifest';
   var exclude = [].concat(options.exclude || []);
   var hasher = crypto.createHash('sha256');
-
-  if (options.timestamp) {
-    contents.push('# Time: ' + new Date());
-  }
-
-  if (options.revision) {
-    contents.push('# Revision: ' + options.revision);
-  }
-
-  contents.push(lineBreak);
-  contents.push('CACHE:');
 
   function writeToManifest(file) {
     if (file.isNull())   return;
@@ -36,7 +26,7 @@ function manifest(options) {
       return;
     }
 
-    contents.push('/' + ((options.relativePath|| '').replace(/([^\/])$/, "$1/") || '')+encodeURI(slash(file.relative)));
+    contents.files.push('/' + ((options.relativePath|| '').replace(/([^\/])$/, "$1/") || '')+encodeURI(slash(file.relative)));
 
     if (options.hash) {
       hasher.update(file.contents, 'binary');
@@ -44,49 +34,17 @@ function manifest(options) {
   }
 
   function endStream() {
-    // Network section
-    options.network = options.network || ['*'];
-    contents.push(lineBreak);
-    contents.push('NETWORK:');
-    options.network.forEach(function (file) {
-      contents.push(encodeURI(file));
-    });
-
-    // Fallback section
-    if (options.fallback) {
-      contents.push(lineBreak);
-      contents.push('FALLBACK:');
-      options.fallback.forEach(function (file) {
-        var firstSpace = file.indexOf(' ');
-        if(firstSpace === -1) {
-          return gutil.log('Invalid format for FALLBACK entry', file);
-        }
-        contents.push(
-          encodeURI(file.substring(0, firstSpace)) +
-          ' ' +
-          encodeURI(file.substring(firstSpace + 1))
-        );
-      });
-    }
-
-    // Settings section
-    if (options.preferOnline) {
-      contents.push(lineBreak);
-      contents.push('SETTINGS:');
-      contents.push('prefer-online');
-    }
-
-    // output hash to cache manifest
-    if (options.hash) {
-      contents.push('\n# hash: ' + hasher.digest("hex"));
-    }
+    if (options.hash)
+      contents.hash = hasher.digest("hex");
+    else
+      contents.hash = new Date();
 
     var cwd = process.cwd();
     var manifestFile = new gutil.File({
       cwd: cwd,
       base: cwd,
       path: path.join(cwd, filename),
-      contents: new Buffer(contents.join(lineBreak))
+      contents: new Buffer(JSON.stringify(contents))
     });
 
     this.emit('data', manifestFile);
